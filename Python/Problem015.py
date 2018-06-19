14fc65aab6820dd3b2438ee1287709ba4680ae07
Caio Almeida <caio.almeida1997@hotmail.com>
2018-06-19 13:37:43 -0300

    Nothing important

--- Python/Problem015.py
+++ Python/Problem015.py
@@ -1,3 +1,6 @@
 from gmpy2 import comb
+
+# Look at Problem015.mkd to understand why the possibilities are given by
+# the combination of (40, 20)
 possibilities = comb(2*20, 20);
 print(possibilities);
---
+++ Python/Problem023.py
@@ -0,0 +1,31 @@
+from itertools import *
+
+def divisors(n):
+    div = [];
+    for i in range(1, n/2 + 1):
+        if(n % i == 0):
+            div.append(i);
+    return div;
+
+def classify(n):
+    div_sum = sum(divisors(n));
+    if div_sum < n:
+        return -1; # Deficient number
+    elif div_sum == n:
+        return 0;  # Perfect number
+    elif div_sum > n:
+        return -1; # Abundant number
+
+limit = 28123; # All numbers above 28123 can be written by the sum of 2 abundant numbers
+abundant_nums = [];
+# for i in range(1, limit):
+#     if abundant(i):
+#         abundant_nums.append(i);
+
+# print(abundant_nums);
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y = [2, 4, 6, 8, 10]
+z = [a for a in x if a not in y]
+for a in combinations(x, 2):
+    print a
+# print z
